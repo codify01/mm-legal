22b8ce5287dde57c463e937dc319f575af5fad1a
codify <amoleuthman@gmail.com>
2025-06-09 16:08:00 +0100

    chore: add commented-out local MongoDB connection string in index.js

--- index.js
+++ index.js
@@ -24,4 +24,6 @@ app.use('/api/content', contentRoutes);
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
     mongoose.connect("mongodb+srv://amoleuthman:Jhmaarsof@cluster0.xkg3cvj.mongodb.net/mindsmaking-legal?retryWrites=true&w=majority&appName=Cluster0")
+    // mongoose.connect("mongodb://admin:Mindsmaking&234!@127.0.0.1:27017/mm-legal")
+    
 })
