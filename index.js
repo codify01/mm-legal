6409a582212ecb66c1c28ebe67ab47a0e1c2c2b0
codify <amoleuthman@gmail.com>
2025-06-09 16:35:24 +0100

    refactor: streamline MongoDB connection logic and clean up commented code

--- index.js
+++ index.js
@@ -11,19 +11,37 @@ app.use(bodyParser.json());
 
 // Routes
 const contentRoutes = require('./src/routes/content.routes');
-app.use('/api/content', contentRoutes);
+app.use('/api/content', contentRoutes)
 
 
 
 
-
+mongoose.connect("mongodb+srv://mindsmakingbrands:Xy1ayvgFIux7dEpO@cluster0.f2ylxo5.mongodb.net/mm-legal?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+    console.log("✅ Connected to MongoDB successfully");
+    
+    app.listen(3000, () => {
+        console.log("🚀 Server is running on port 3000");
+    });
+})
+.catch((error) => {
+    console.error("❌ Error connecting to MongoDB:", error);
+});
 
 
 
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
-    mongoose.connect("mongodb+srv://amoleuthman:Jhmaarsof@cluster0.xkg3cvj.mongodb.net/mindsmaking-legal?retryWrites=true&w=majority&appName=Cluster0")
-    // mongoose.connect("mongodb://admin:Mindsmaking&234!@127.0.0.1:27017/mm-legal")
+    // mongoose.connect("mongodb+srv://amoleuthman:Jhmaarsof@cluster0.xkg3cvj.mongodb.net/mindsmaking-legal?retryWrites=true&w=majority&appName=Cluster0")
+    // mongoose.connect("mongodb+srv://mindsmakingbrands:Xy1ayvgFIux7dEpO@cluster0.f2ylxo5.mongodb.net/mm-legal?retryWrites=true&w=majority&appName=Cluster0")
+    // .then(() => {
+    //     console.log("Connected to MongoDB successfully");
+    // })
+    // .catch((error) => {
+    //     console.error("Error connecting to MongoDB:", error);
+    // });
+    // // mongoose.connect("mongodb://admin:Mindsmaking&234!@127.0.0.1:27017/mm-legal")c
+    // // Xy1ayvgFIux7dEpO
     
 })
